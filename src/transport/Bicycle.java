4d3e93f3a6acbffcf3118c384612788fc3835de4
Timofey <timofey.sharipov@gmail.com>
2023-10-12 16:52:59 +0300

    first

---
+++ src/transport/Bicycle.java
@@ -0,0 +1,11 @@
+package transport;
+
+public class Bicycle {
+
+    public String modelName;
+    public int wheelsCount;
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+}
